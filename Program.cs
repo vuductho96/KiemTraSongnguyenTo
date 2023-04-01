a202186fbeca54f6b471a3ebfa78179808578919
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-01 14:21:51 +0700

    Kiemtrasonguyento

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace KiemTrAsONGYTENtO;
+class KiemTrA
+{
+
+    static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("Enter Number");
+        number = int.Parse(Console.ReadLine());
+        if (number < 2) {
+            Console.WriteLine(number + " is not a prime");
+
+    }
+        else
+            {
+            int i = 2;
+            bool check = true;
+            while (i <= Math.Sqrt(number))
+            {
+                if (number % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+            {
+                Console.WriteLine(number + " is a prime");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a prime");
+            }
+        }
+
+
+    }}
+
+
+
